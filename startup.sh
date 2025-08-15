4f9b8272bfc6be6b2e29cd24057f435de5296b32
Sujay Sharma <sujay.sharma@mca.christuniversity.in>
2025-08-15 18:24:26 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# STEP 1: Install Google Chrome
-echo "INSTALLING GOOGLE CHROME"
+# STEP 1: Install system dependencies for Chromedriver ⚙️
+echo "INSTALLING SYSTEM DEPENDENCIES"
 apt-get update
-apt-get install -y wget unzip
+apt-get install -y libnss3 libgconf-2-4 libfontconfig1
+
+# STEP 2: Install Google Chrome
+echo "INSTALLING GOOGLE CHROME"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install
 rm google-chrome-stable_current_amd64.deb
 echo "GOOGLE CHROME INSTALLATION COMPLETE"
 
-# STEP 2: Start the Gunicorn server
+# STEP 3: Start the Gunicorn server
 echo "STARTING GUNICORN"
-gunicorn --bind=0.0.0.0 --timeout 120 --workers=1 app:app
+gunicorn --bind=0.0.0.0 --timeout 600 --workers=1 app:app
